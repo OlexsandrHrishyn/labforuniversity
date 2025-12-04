1e4848e8afba12aa1d5efbde1e503030c971a4f6
OlexsandrHrishyn <153458201+OlexsandrHrishyn@users.noreply.github.com>
2025-12-04 17:20:17 +0200

    Add files via upload

---
+++ hrishyn_lab1/Animal.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Animal.Common;
+using System;
+
+namespace Animal.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var service = new CatCrudService();
+
+var Cat1 = new Cat("Cat", "Mammal", "Carnivora", "Bobik") { Breed = "Dachshund" };
+    var Cat2 = new Cat("Cat", "Mammal", "Carnivora", "Sharik") { Breed = "Shepherd" };
+
+         var id1 = service.Create(Cat1);
+                var id2 = service.Create(Cat2);
+
+                    Console.WriteLine("\n[READ ALL]");
+            foreach (var d in service.ReadAll())
+                    Console.WriteLine($" - {d.Nickname}, {d.Breed}");
+
+            var newCat = new Cat("Cat", "Mammal", "Carnivora", "Bobik") { Breed = "Corgi" };
+            service.Update(id1, newCat);
+
+            service.Delete(id2);
+
+            Console.WriteLine("\n[READ ALL after delete]");
+            foreach (var d in service.ReadAll())
+                Console.WriteLine($" - {d.Nickname}, {d.Breed}");
+        }
+    }
+}
---
+++ hrishyn_lab1/Animal.Console/catCrudService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animal.Common
+{
+    public class CatCrudService
+    {
+        private readonly Dictionary<Guid, Cat> Cats = new();
+
+        public Guid Create(Cat Cat)
+        {
+            var id = Guid.NewGuid();
+            _Cats[id] = Cat;
+            Console.WriteLine($"[CREATE] {Cat.Nickname} add with Id {id}");
+            return id;
+        }
+
+        public Cat? Read(Guid id)
+        {
+            return _Cats.TryGetValue(id, out var Cat) ? Cat : null;
+        }
+
+        public IEnumerable<Cat> ReadAll()
+        {
+            return _Cats.Values;
+        }
+
+        public void Update(Guid id, Cat newCat)
+        {
+            if (_Cats.ContainsKey(id))
+            {
+                _Cats[id] = newCat;
+                Console.WriteLine($"[UPDATE] Cat with Id {id} update on{newCat.Nickname}");
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            if (_Cats.Remove(id, out var Cat))
+            {
+                Console.WriteLine($"[DELETE] {Cat.Nickname} "); 
+            }
+        }
+    }
+}
